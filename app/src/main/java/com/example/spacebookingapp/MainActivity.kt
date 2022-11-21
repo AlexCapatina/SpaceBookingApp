62ac869473f1e5dd14322520194d5f994bd2f347
Alex <oravetzal@yahoo.com>
2022-11-20 19:42:52 -0500

    Added search function

--- app/src/main/java/com/example/spacebookingapp/MainActivity.kt
+++ app/src/main/java/com/example/spacebookingapp/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.spacebookingapp
 
+import android.app.Activity
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.Surface
+import android.widget.SearchView
 import org.w3c.dom.Text
 import java.lang.reflect.Modifier
 
@@ -11,6 +17,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+        return true
+    }
+
+}
+class SearchResultsActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            //use the query to search your data somehow
+        }
     }
 }
 
