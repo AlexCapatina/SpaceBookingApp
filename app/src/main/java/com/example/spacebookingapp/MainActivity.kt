65a04d05315dae607291640e7939248718fac04a
Alex <oravetzal@yahoo.com>
2022-11-20 19:13:41 -0500

    Added background image

--- app/src/main/java/com/example/spacebookingapp/MainActivity.kt
+++ app/src/main/java/com/example/spacebookingapp/MainActivity.kt
@@ -10,32 +10,8 @@ import java.lang.reflect.Modifier
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
-            GreetingCardTheme{
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ){
-                    Greeting("Android")
-                }
-            }
-        }
-
+        setContentView(R.layout.activity_main);
     }
 }
 
-@Composable
-fun Greeting(name: String){
-    Surface(color = Color.CYAN){
-        Text(text = "Hi, welcome to SpaceBooking!", modifier = Modifier.padding(24.dp))
-    }
 
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview(){
-    GreetingCardTheme{
-        Greeting( "Android")
-    }
-}
